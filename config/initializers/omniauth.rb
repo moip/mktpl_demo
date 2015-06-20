589051f8d03776299bc52219f5f05ddc72516876
Rodrigo Saito <rodrigo.saito@gmail.com>
2015-06-20 09:31:03 -0300

    fix redirect_uri

--- config/initializers/omniauth.rb
+++ config/initializers/omniauth.rb
@@ -2,7 +2,7 @@ Rails.application.config.middleware.use OmniAuth::Builder do
   provider :moip, ENV["CLIENT_ID"], ENV["CLIENT_SECRET"],
     {
       environment: :sandbox,
-      redirect_uri: "http://moipdevcommerce.herokuapp.com",
+      redirect_uri: "https://moipdevcommerce.herokuapp.com",
       scope: "CREATE_ORDER",
     }
 end
