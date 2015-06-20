f3a2d8988a0d1b23e8cb6ae964a2d1d8910fa099
Rodrigo Saito <rodrigo.saito@gmail.com>
2015-06-20 09:28:49 -0300

    Fix omniauth properties

--- config/initializers/omniauth.rb
+++ config/initializers/omniauth.rb
@@ -1,8 +1,8 @@
 Rails.application.config.middleware.use OmniAuth::Builder do
-  provider :moip, "APP-N8PEEE7AV17P", "q6zg0z8qxwmjzr8f0kt11qnvtwl8wg8",
+  provider :moip, ENV["CLIENT_ID"], ENV["CLIENT_SECRET"],
     {
       environment: :sandbox,
-      redirect_uri: "http://localhost:5000",
+      redirect_uri: "http://moipdevcommerce.herokuapp.com",
       scope: "CREATE_ORDER",
     }
 end
